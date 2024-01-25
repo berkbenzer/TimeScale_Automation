71c1e9d9c484a83789f1827decfeb096fae53d4e
Berk Benzer <52455783+berkbenzer@users.noreply.github.com>
2024-01-25 20:34:00 +0100

    Update 1_mysql.sql

--- 1_mysql.sql
+++ 1_mysql.sql
@@ -2,9 +2,9 @@ connect python_test;
 select who,vm, action, date_format(start_time,'%d-%m-%Y') as start_t, \
         date_format(end_time, '%d-%m-%Y') as end_t, \
         NULL AS chunk_stat \
-        from python_test.launcher_history \
+        from python_test.table_name \
         where 1=1 \
-        and vm in ('dwh-ps','dwh-ps2')\
+        and vm in ('proc_1','proc_2')\
         and action = 'stop' \
         and who != 'system'\
         group by who,vm, action,start_t,end_t \
